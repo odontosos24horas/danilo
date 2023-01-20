8317102cd6e19f399a616179fbd3a3498c40ac59
Pedro Duarte <pedrohlduarte@gmail.com>
2023-01-20 15:49:41 -0300

    feat: add gtm

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,24 +14,19 @@ export default class MyDocument extends Document {
             content="implantes em BH, periodontia BH, implante dentário, implantes dentários BH"
           />
           <link rel="icon" href="/favicon.png" />
-          {/* <script async src="https://www.googletagmanager.com/gtag/js?id=GTM-PPHPCBH" />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=GTM-TPK3DCT" />
           <script
             dangerouslySetInnerHTML={{
               __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'GTM-PPHPCBH');
+              gtag('config', 'GTM-TPK3DCT');
             `
             }}
-          /> */}
+          />
         </Head>
         <body>
-          {/* <noscript>
-            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PPHPCBH"
-              height="0" width="0">
-            </iframe>
-          </noscript> */}
           <Main />
           <NextScript />
         </body>
